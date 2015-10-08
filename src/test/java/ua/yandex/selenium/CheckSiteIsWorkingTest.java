c138723a550727a44bd6004c6c48c740c5ad312d
Andrii Rodionov <Andrii.Rodionov@gmail.com>
2015-10-08 11:58:55 +0300

    Updateed

--- src/test/java/ua/yandex/selenium/CheckSiteIsWorkingTest.java
+++ src/test/java/ua/yandex/selenium/CheckSiteIsWorkingTest.java
@@ -18,7 +18,7 @@ public class CheckSiteIsWorkingTest {
   @Before
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
-    baseUrl = "http://localhost:8080";
+    baseUrl = "http://localhost:8181";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
